240c4076f0d086f20dec7d93ae746993ea5ee748
Deeppatel-4032 <deeppatel7868@gmail.com>
2025-03-04 12:11:46 +0530

    db connection

--- db/movie_db_file.js
+++ db/movie_db_file.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/movies_db")
+mongoose.connect(process.env.MONGODB_URL)
     .then(() => {
         console.log("db is connected...........!");
     })
